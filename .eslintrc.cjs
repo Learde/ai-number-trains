5512a35fdc3506f15d95f6c159f8478d304fe736
Learde <leardeleo@gmail.com>
2023-10-14 18:19:02 +0300

    Add default files to proj

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,27 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    root: true,
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    settings: {
+        'import/resolver': {
+            alias: {
+                map: [['@', './src']]
+            },
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                moduleDirectory: ['node_modules', 'src/']
+            }
+        },
+        'prettier/prettier': [
+            'error',
+            {
+                tabWidth: 4,
+                endOfLine: 'auto',
+            }
+        ]
+    }
 }
